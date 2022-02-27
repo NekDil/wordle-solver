3c8d1dd9f41c2f7d2a8674020157b6492694847c
Sandeep Verma <sandeep@Sandeeps-Mini.local>
2022-02-27 11:20:35 -0800

    Reformatted code and removed unused dependency

--- src/main/java/me/verma/app/WordleSolver.java
+++ src/main/java/me/verma/app/WordleSolver.java
@@ -19,17 +19,14 @@ import static me.verma.app.Utils.starters;
 
 /**
  * A simple wordle solver
- *
  */
-public class WordleSolver
-{
-    private static final Logger logger = Logger.getGlobal();
-
-    private static Set<String> words;
-    private static final Table<Integer, Integer, Set<String>> wordTable = HashBasedTable.create();
+public class WordleSolver {
     public static final int WORD_LENGTH = 5;
+    private static final Logger logger = Logger.getGlobal();
+    private static final Table<Integer, Integer, Set<String>> wordTable = HashBasedTable.create();
+    private static Set<String> words;
 
-    private static void getWordsOf5() throws IOException {
+    private static void getWordsFromDict() throws IOException {
         words = Files.lines(Paths.get("/usr/share/dict/words"))
                 .filter(s -> s.length() == WORD_LENGTH)
                 .map(String::toLowerCase)
@@ -38,8 +35,8 @@ public class WordleSolver
     }
 
     private static void tabulate() {
-        for(int i = 0; i < 26; ++i) {
-            for(int j = 0; j < WORD_LENGTH; ++j) {
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < WORD_LENGTH; ++j) {
                 wordTable.put(i, j, new HashSet<>());
             }
         }
@@ -60,37 +57,37 @@ public class WordleSolver
     private static Set<String> guessBestNextWords(final String currentWord, final String hint) {
 
         // utilize hint to best guess next words with score. B is still BLANK, G is GREEN, Y is YELLOW
-        if(currentWord.isEmpty() || hint.isEmpty()) {
+        if (currentWord.isEmpty() || hint.isEmpty()) {
             return Set.of(randomPopularWord().toLowerCase(Locale.ROOT));
         } else {
             Map<Character, Integer> exactMatches = new HashMap<>();
             Map<Character, Integer> approxMatches = new HashMap<>();
             Map<Character, Integer> mismatches = new HashMap<>();
 
-            for(int pos = 0; pos < WORD_LENGTH; ++pos) {
+            for (int pos = 0; pos < WORD_LENGTH; ++pos) {
                 char hintChar = hint.charAt(pos);
                 char wordChar = currentWord.charAt(pos);
-                if(hintChar == 'b') {
-                    if(!(exactMatches.containsKey(wordChar) || approxMatches.containsKey(wordChar)))
+                if (hintChar == 'b') {
+                    if (!(exactMatches.containsKey(wordChar) || approxMatches.containsKey(wordChar)))
                         mismatches.put(wordChar, pos);
-                } else if(hintChar == 'g') {
+                } else if (hintChar == 'g') {
                     exactMatches.put(wordChar, pos);
                     mismatches.remove(wordChar);
-                } else if(hint.charAt(pos) == 'y') {
+                } else if (hint.charAt(pos) == 'y') {
                     approxMatches.put(wordChar, pos);
                     mismatches.remove(wordChar);
                 }
             }
 
             // mismatch removal
-            for(Entry<Character, Integer> mismatch : mismatches.entrySet()) {
+            for (Entry<Character, Integer> mismatch : mismatches.entrySet()) {
                 words.removeIf(w -> containsChar(w, mismatch.getKey()));
             }
             logger.info("Remaining " + words.size() + " words after mismatch removal!");
 
             // retain only exact matches in words list
             Set<String> exacts = retainOnlyExactMatches(currentWord, exactMatches);
-            if(!exacts.isEmpty()) words.retainAll(exacts);
+            if (!exacts.isEmpty()) words.retainAll(exacts);
             logger.info("Remaining " + words.size() + " words after green inclusion!");
 
             // make sure they have approx matches but not at exact position
@@ -106,7 +103,7 @@ public class WordleSolver
             final Map<Character, Integer> exactMatches) {
         // find all green words at position
         Set<String> greens = new HashSet<>();
-        for(Entry<Character, Integer> green : exactMatches.entrySet()) {
+        for (Entry<Character, Integer> green : exactMatches.entrySet()) {
             greens.addAll(Objects.requireNonNull(wordTable.get(green.getKey() - 'a', green.getValue())));
         }
         final Pattern pattern = Pattern.compile(getRegex(currentWord, exactMatches));
@@ -119,7 +116,7 @@ public class WordleSolver
 
     private static boolean approxMatchFilter(Map<Character, Integer> approxMatches, String word) {
         boolean remove = false;
-        for(char c : approxMatches.keySet()) {
+        for (char c : approxMatches.keySet()) {
             if (!containsChar(word, c) && word.indexOf(c) != approxMatches.get(c)) {
                 return true;
             }
@@ -129,9 +126,9 @@ public class WordleSolver
 
     private static String getRegex(String currentWord, Map<Character, Integer> exactMatches) {
         StringBuilder regex = new StringBuilder();
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             Character c = currentWord.charAt(i);
-            if(exactMatches.containsKey(c) && i == exactMatches.get(c)) regex.append(c);
+            if (exactMatches.containsKey(c) && i == exactMatches.get(c)) regex.append(c);
             else regex.append(".");
         }
         return regex.toString();
@@ -139,8 +136,8 @@ public class WordleSolver
 
     public static void main(String[] args) {
         try {
-            getWordsOf5();
-        } catch(final Exception e) {
+            getWordsFromDict();
+        } catch (final Exception e) {
             logger.log(Level.SEVERE, "failed to laod dict", e);
         }
         tabulate();
@@ -150,7 +147,7 @@ public class WordleSolver
         String currentInput = "";
         System.out.println(guessBestNextWords(initialState.toLowerCase(), hint.toLowerCase()));
 
-        while(!currentInput.equals("exit")) {
+        while (!currentInput.equals("exit")) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter current input: ");
             currentInput = scanner.nextLine();
